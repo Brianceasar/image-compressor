c41413cd2e9fdf5d6bc304d23f05cf2005b8a48f
Brianceasar <95188858+Brianceasar@users.noreply.github.com>
2023-03-30 10:35:39 +0300

    fixed OsEror

--- app.py
+++ app.py
@@ -9,12 +9,21 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
+        # Get the uploaded image
         image = request.files['image'].read()
         image = Image.open(BytesIO(image))
 
+        # Get file extension of uploaded image
+        file_ext = request.files['image'].filename.split('.')[-1]
+
         # compress the image
         output = BytesIO()
-        image.save(output, format='JPEG', quality=50)
+        if image.mode !='RGB':
+            image = image.convert('RGB')
+        if file_ext.lower() == 'png':
+            image.save(output, format='PNG', optimize=True)
+        else:
+            image.save(output, format="JPEG", quality=50)
         output.seek(0)
 
         # convert the compressed image to base64 for display
